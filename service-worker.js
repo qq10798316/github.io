45945e800ecfb189f768b16d55fd8c42ee9b049e
houminglong <123456>
2021-01-25 17:09:24 +0800

    deploy

---
+++ service-worker.js
@@ -0,0 +1,148 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "99148ae78d73df50fea3c8370ca020ff"
+  },
+  {
+    "url": "accumulate/index.html",
+    "revision": "fc86eab94e00e5e7c817747c2f9843e5"
+  },
+  {
+    "url": "algorithm/index.html",
+    "revision": "d3a7076692ca53792ae228670873b3eb"
+  },
+  {
+    "url": "assets/css/0.styles.8eb0d87d.css",
+    "revision": "4323bac446eb6bba0eae56e8fc5a8a35"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/2.858e80da.js",
+    "revision": "59b9e09855a16d382db7d8ff58d74b0d"
+  },
+  {
+    "url": "assets/js/3.dad9bf50.js",
+    "revision": "79d535a9dd7f81156e73c93b63860feb"
+  },
+  {
+    "url": "assets/js/4.5b9a6c8a.js",
+    "revision": "0ec7a0c397ca05efbe3f6a678e620d48"
+  },
+  {
+    "url": "assets/js/5.e95ce13d.js",
+    "revision": "2b7d2dba8c3f812c3c2732a9a74b7e2d"
+  },
+  {
+    "url": "assets/js/6.a4fdd253.js",
+    "revision": "72d69bbd1e73426e350322ccd742d8ba"
+  },
+  {
+    "url": "assets/js/7.b8ae3c43.js",
+    "revision": "3803aa4625b66eb33a316c4938baf13a"
+  },
+  {
+    "url": "assets/js/8.12f716cf.js",
+    "revision": "96e584c7b994a13aa55791c93c33f368"
+  },
+  {
+    "url": "assets/js/app.9f73f503.js",
+    "revision": "684c6183238c9c475fe37203d9d40252"
+  },
+  {
+    "url": "guide.html",
+    "revision": "bde04f51445b7718ae530590e5e1e707"
+  },
+  {
+    "url": "images/eg1.png",
+    "revision": "b6e00451aa6e2fb07803babc3be44fe3"
+  },
+  {
+    "url": "images/eg12.png",
+    "revision": "716c3bede731ed6eecb026377f31aac1"
+  },
+  {
+    "url": "images/eg13.png",
+    "revision": "022c1e3380b12aa21dd816cd4f1aeaae"
+  },
+  {
+    "url": "images/eg14.png",
+    "revision": "c0b03bb9c818398235ae5ac24fe91204"
+  },
+  {
+    "url": "images/eg2.png",
+    "revision": "1c31890ab04672b5b9daad1f8216a89b"
+  },
+  {
+    "url": "images/eg3.png",
+    "revision": "c3145209954ef832a2c62740a2133a8b"
+  },
+  {
+    "url": "images/eg4.png",
+    "revision": "7b4a9dd311c20e60f1f67205845deca1"
+  },
+  {
+    "url": "images/eg5.png",
+    "revision": "d70a372965f420d2faaf40259dce9a1c"
+  },
+  {
+    "url": "images/eg6.png",
+    "revision": "64c96504fe466f4baa99423a71573892"
+  },
+  {
+    "url": "images/eg7.png",
+    "revision": "5814399d3ba9bdcedb588e2b65059e67"
+  },
+  {
+    "url": "images/eg8.png",
+    "revision": "cd30b75ad80917829755a2693a374b02"
+  },
+  {
+    "url": "images/photo.jpg",
+    "revision": "d4d77052d44bea42bbfc4dba196bde93"
+  },
+  {
+    "url": "index.html",
+    "revision": "abca6774ba1582bebdbef38d6303db70"
+  },
+  {
+    "url": "others/index.html",
+    "revision": "fa844874fff65dec7b04d5f47dda229e"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
